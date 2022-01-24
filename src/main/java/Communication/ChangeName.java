3dd2849cdeaceb430f7bf1dd01242e778f9aa6e3
NailikLN <lenezetkilian@gmail.com>
2022-01-25 00:32:35 +0100

    Test 2

--- src/main/java/Communication/ChangeName.java
+++ src/main/java/Communication/ChangeName.java
@@ -29,7 +29,7 @@ public class ChangeName extends TypeOfMessage {
         byte buffer[] = new byte[LENGTH_BUFFER];
         buffer[0] = this.type.to_byte();
         byte[] nameBuffer = this.name.getBytes(StandardCharsets.UTF_8);
-        buffer[1] = (byte) (min(nameBuffer.length, 254)-128);
+        buffer[1] = (byte) (nameBuffer.length);
         System.arraycopy(nameBuffer, 0, buffer,2, nameBuffer.length);
         DatagramPacket packet = new DatagramPacket(buffer,LENGTH_BUFFER);
         packet.setAddress(this.adress);
